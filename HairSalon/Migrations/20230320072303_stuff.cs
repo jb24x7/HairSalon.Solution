30e872dbb7ff31b4992582560b00beeeaac978c9
James Provance <jb24x7@gmail.com>
2023-03-20 00:23:28 -0700

    add migration

---
+++ HairSalon/Migrations/20230320072303_stuff.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HairSalon.Migrations
+{
+    public partial class stuff : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
